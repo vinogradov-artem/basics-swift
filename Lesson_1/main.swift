57d56dd72b7e913869a4b3128e6060a3b71e792c
vinogradov-artem <vinogradov4113@mail.ru>
2021-10-16 15:58:32 +0300

    Сделал 2 дз

--- Lesson_1/main.swift
+++ Lesson_1/main.swift
@@ -1,41 +1,51 @@
 import Foundation
 
-//MARK: -  Решение квадратного уравнения ax2+bx+c=0
+//MARK: - Написать функцию, которая определяет, четное число или нет.
 
-let a = 3.0
-let b = 12.0
-let c = -5.0
-let x1: Double
-let x2: Double
-let discriminant = b * b - 4 * a * c
+func isEvenNumber(number: Int) -> Bool { number % 2 == 0 }
 
-if discriminant > 0 {
-    x1 = -b + sqrt(discriminant) / (2 * a)
-    x2 = -b - sqrt(discriminant) / (2 * a)
-    print("Уравнение имеет 2 корня: x1 = \(x1), x2 = \(x2)")
-} else if (discriminant == 0) {
-    x1 = -b / (2 * a)
-    print("Уравнение имеет 1 корень: x1 = \(x1)")
-} else {
-    print("Нет корней")
+
+//MARK: - Написать функцию, которая определяет, делится ли число без остатка на 3.
+
+func isNumber(_ number: Int, dividedBy divider: Int) -> Bool {
+    return number % divider == 0
 }
 
-//MARK: - Нахождение периметра, площади и гипотенузы треугольника
+//MARK: - Создать возрастающий массив из 100 чисел.
 
-let katit1: (Double) = 3
-let katit2: (Double) = 4
-let hypotenuse = sqrt((Double)(katit1 * katit2) + (Double)(katit2 * katit1))
-let perimeter = katit1 + katit2 + hypotenuse
-let area = (katit1 * katit2) / 2
 
-//MARK: - Сумма вклада за 5 лет
+var array = [Int]()
+for i in 0...99 {
+    array.append(i)
+}
+var otherArray = [0...99]
 
-let depositAmount = 100_000.00
-let percent = 12.00
-var profit = depositAmount * percent / 100
-let depositAmountForTheFirstYear = depositAmount + profit
-let DepositAmountForTheSecondYear = depositAmountForTheFirstYear * (percent / 100) + depositAmountForTheFirstYear
-let DepositAmountForTheThirdYear = DepositAmountForTheSecondYear * (percent / 100) + DepositAmountForTheSecondYear
-let DepositAmountForTheFourthYear = DepositAmountForTheThirdYear * (percent / 100) + DepositAmountForTheThirdYear
-let DepositAmountForTheFifthYear = DepositAmountForTheFourthYear * (percent / 100) + DepositAmountForTheFourthYear
-print ("Сумма вклада через 5 лет равна",  (DepositAmountForTheFifthYear))
+//MARK: - Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
+
+var index = 0
+while index < array.count {
+
+    if array[index] % 2 == 0 || array[index] % 3 != 0 {
+        array.remove(at: index)
+        index -= 1
+    }
+    index += 1
+}
+print(array)
+
+
+//MARK: - Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов.
+
+func fiboArray(index: Int) -> [Int] {
+
+    var array = [0,1]
+    array = [0,1]
+
+    for index in 2..<index {
+       array.append(array[index - 2] + array[index - 1])
+    }
+
+    return array
+}
+
+print(fiboArray(index: 48))
