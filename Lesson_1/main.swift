b4400bed51bb80c7501cd10b31ce5327eaf7f461
vinogradov-artem <vinogradov4113@mail.ru>
2021-10-25 13:58:24 +0300

    Сделал дз 3

--- Lesson_1/main.swift
+++ Lesson_1/main.swift
@@ -1,51 +1,199 @@
 import Foundation
 
-//MARK: - Написать функцию, которая определяет, четное число или нет.
 
-func isEvenNumber(number: Int) -> Bool { number % 2 == 0 }
+struct SportCar {
+    let carBrand: String
+    let yearOfRelease: Int
+    let trunkVolume: Double
+    var engineRunning: Bool
+    var windowsIsOpen: Bool
+    var filledTrunkVolume: Double
 
-
-//MARK: - Написать функцию, которая определяет, делится ли число без остатка на 3.
-
-func isNumber(_ number: Int, dividedBy divider: Int) -> Bool {
-    return number % divider == 0
-}
-
-//MARK: - Создать возрастающий массив из 100 чисел.
-
-
-var array = [Int]()
-for i in 0...99 {
-    array.append(i)
-}
-var otherArray = [0...99]
-
-//MARK: - Удалить из этого массива все четные числа и все числа, которые не делятся на 3.
-
-var index = 0
-while index < array.count {
-
-    if array[index] % 2 == 0 || array[index] % 3 != 0 {
-        array.remove(at: index)
-        index -= 1
-    }
-    index += 1
-}
-print(array)
-
-
-//MARK: - Написать функцию, которая добавляет в массив новое число Фибоначчи, и добавить при помощи нее 50 элементов.
-
-func fiboArray(index: Int) -> [Int] {
-
-    var array = [0,1]
-    array = [0,1]
-
-    for index in 2..<index {
-       array.append(array[index - 2] + array[index - 1])
+    enum Action {
+        case startEngine
+        case stopEngine
+        case openWindow
+        case closeWindow
+        case fillTrunk(cargoVolume: Double)
+        case emptyTrunk(cargoVolume: Double)
     }
 
-    return array
+    mutating func makeAction(action: Action) {
+        switch action {
+        case .startEngine:
+            engineRunning = true
+            print("Двигатель запущен")
+        case .stopEngine:
+            engineRunning = false
+            print("Двигатель заглушен")
+        case .openWindow:
+            windowsIsOpen = true
+            print("Окна открыты")
+        case .closeWindow:
+            windowsIsOpen = false
+            print("Окна закрыты")
+        case .fillTrunk(let cargoVolume):
+            if cargoVolume <= (trunkVolume - filledTrunkVolume) {
+                filledTrunkVolume += cargoVolume
+                print("Багажник загружен: \(filledTrunkVolume) л")
+            } else {
+                print("Объем багажника не позволяет поместить весь груз. Заполненный объем: \(trunkVolume - filledTrunkVolume)")
+            }
+        case .emptyTrunk(let cargoVolume):
+            if cargoVolume <= filledTrunkVolume {
+                print("Недостаточное количество груза в багажнике")
+            } else {
+                filledTrunkVolume -= cargoVolume
+                print("Багажник разгружен")
+            }
+        }
+    }
+
+    func printInfo() {
+        print("Марка авто: \(self.carBrand)")
+        print("Год выпуска: \(self.yearOfRelease)")
+        print("Объем багажника: \(self.trunkVolume)")
+        print("Состояние двигателя: \(self.engineRunning ? "Запущен" : "Заглушен")")
+        print("Положение окон: \(self.windowsIsOpen ? "Открыты" : "Закрыты")")
+        print("Загрузка багажника: \(self.trunkVolume)")
+    }
 }
 
-print(fiboArray(index: 48))
+var lancia = SportCar(carBrand: "lancia",
+                    yearOfRelease: 2020,
+                    trunkVolume: 300,
+                    engineRunning: true,
+                    windowsIsOpen: true,
+                    filledTrunkVolume: 100)
+
+lancia.printInfo()
+lancia.makeAction(action: .closeWindow)
+lancia.makeAction(action: .fillTrunk(cargoVolume: 50))
+lancia.makeAction(action: .fillTrunk(cargoVolume: 200))
+print(lancia.filledTrunkVolume)
+
+var subaru = SportCar(carBrand: "subaru",
+                     yearOfRelease: 2021,
+                     trunkVolume: 500,
+                     engineRunning: false,
+                     windowsIsOpen: false,
+                     filledTrunkVolume: 0)
+
+subaru.printInfo()
+subaru.makeAction(action: .startEngine)
+subaru.makeAction(action: .fillTrunk(cargoVolume: 200))
+subaru.makeAction(action: .fillTrunk(cargoVolume: 200))
+print(subaru.filledTrunkVolume)
+
+var nissan = SportCar(carBrand: "nissan",
+                      yearOfRelease: 2019,
+                      trunkVolume: 400,
+                      engineRunning: true,
+                      windowsIsOpen: false,
+                      filledTrunkVolume: 0)
+
+nissan.printInfo()
+nissan.makeAction(action: .startEngine)
+nissan.makeAction(action: .closeWindow)
+nissan.makeAction(action: .fillTrunk(cargoVolume: 200))
+nissan.makeAction(action: .fillTrunk(cargoVolume: 300))
+print(nissan.filledTrunkVolume)
+
+//MARK:-
+
+struct TrunkCar {
+    let carBrand: String
+    let yearOfRelease: Int
+    let bodyVolume: Double
+    var engineRunning: Bool
+    var windowsIsOpen: Bool
+    var filledBodyVolume: Double
+
+    enum Action {
+        case startEngine
+        case stopEngine
+        case openWindow
+        case closeWindow
+        case fillBody(cargoVolume: Double)
+        case emptyBody(cargoVolume: Double)
+    }
+
+    mutating func makeAction(action: Action) {
+        switch action {
+        case .startEngine:
+            engineRunning = true
+            print("Двигатель запущен")
+        case .stopEngine:
+            engineRunning = false
+            print("Двигатель заглушен")
+        case .openWindow:
+            windowsIsOpen = true
+            print("Окна открыты")
+        case .closeWindow:
+            windowsIsOpen = false
+            print("Окна закрыты")
+        case .fillBody(let cargoVolume):
+            if cargoVolume <= (bodyVolume - filledBodyVolume) {
+                filledBodyVolume += cargoVolume
+                print("Багажник загружен: \(filledBodyVolume) л")
+            } else {
+                print("Объем багажника не позволяет поместить весь груз. Заполненный объем: \(bodyVolume - filledBodyVolume)")
+            }
+        case .emptyBody(let cargoVolume):
+            if cargoVolume <= filledBodyVolume {
+                print("Недостаточное количество груза в багажнике")
+            } else {
+                filledBodyVolume -= cargoVolume
+                print("Багажник разгружен")
+            }
+        }
+    }
+
+    func printInfo() {
+        print("Марка авто: \(self.carBrand)")
+        print("Год выпуска: \(self.yearOfRelease)")
+        print("Объем багажника: \(self.bodyVolume)")
+        print("Состояние двигателя: \(self.engineRunning ? "Запущен" : "Заглушен")")
+        print("Положение окон: \(self.windowsIsOpen ? "Открыты" : "Закрыты")")
+        print("Загрузка багажника: \(self.bodyVolume)")
+    }
+}
+
+var ural = TrunkCar(carBrand: "ural",
+                    yearOfRelease: 2020,
+                    bodyVolume: 3000,
+                    engineRunning: true,
+                    windowsIsOpen: true,
+                    filledBodyVolume: 0)
+
+ural.printInfo()
+ural.makeAction(action: .closeWindow)
+ural.makeAction(action: .fillBody(cargoVolume: 2000))
+ural.makeAction(action: .fillBody(cargoVolume: 3010))
+print(ural.filledBodyVolume)
+
+var liaz = TrunkCar(carBrand: "liaz",
+                     yearOfRelease: 2021,
+                     bodyVolume: 1200,
+                     engineRunning: false,
+                     windowsIsOpen: false,
+                     filledBodyVolume: 0)
+
+liaz.printInfo()
+liaz.makeAction(action: .startEngine)
+liaz.makeAction(action: .fillBody(cargoVolume: 200))
+liaz.makeAction(action: .fillBody(cargoVolume: 800))
+print(liaz.filledBodyVolume)
+
+var iveco = TrunkCar(carBrand: "iveco",
+                     yearOfRelease: 2019,
+                     bodyVolume: 1300,
+                     engineRunning: true,
+                     windowsIsOpen: false,
+                     filledBodyVolume: 0)
+
+iveco.printInfo()
+iveco.makeAction(action: .stopEngine)
+iveco.makeAction(action: .fillBody(cargoVolume: 350))
+iveco.makeAction(action: .fillBody(cargoVolume: 750))
+print(iveco.filledBodyVolume)
